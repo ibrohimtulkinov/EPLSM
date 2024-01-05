6ef4366c46732a44a3abbc0bfeff5a32f881dda6
ibrohim <tulkinovibrohim7@gmail.com>
2024-01-05 18:39:51 +0500

    Power

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -193,14 +193,17 @@ function Contact() {
 
 
 
-            <div className='text-center mt-5'>
-                <img src={map} alt="" className='col-md-6 col-sm-8 col-10' />
+            <div className="map-div mt-5" >
+                <div className="map">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d95942.48002517321!2d69.19696639704058!3d41.28269707463922!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae8b0cc379e9c3%3A0xa5a9323b4aa5cb98!2z0KLQsNGI0LrQtdC90YIsINCj0LfQsdC10LrQuNGB0YLQsNC9!5e0!3m2!1sru!2s" width="100%" height="300" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
             </div>
 
 
             <End />
 
 
+            {/* <img src={map} alt="" className='col-md-6 col-sm-8 col-10' /> */}
 
         </>
     )
